9de41699f5314b7bb035d61f5547620271c6edc9
etomaro <python.helloworld111@gmail.com>
2021-12-27 14:49:45 +0900

    4-fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,57 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+    // スクリーンアドレスをR0に保存
+    @SCREEN    // @16384
+    D=A 
+    @R0
+    M=D
+    // キーボードアドレスの値が0かどうか調べる(入力があるかどうか)
+    @KBD    // @24576
+    D=M
+    @FILLWHITE
+    D;JEQ    // 0の場合ジャンプ
+    @FILLBLACK
+    0;JMP    // 0ではない場合ジャンプ
+
+(FILLWHITE)
+    // R0の値によってジャンプ
+    @R0
+    D=M
+    @24575    // スクリーンの最後のマップメモリアドレス
+    D=D-A    // R0-24575
+    @LOOP
+    D;JEQ    // 0の場合ジャンプ
+    // R0に保存されているアドレスの値を0にする
+    @R0
+    A=M
+    M=0
+    //  R0をインクリメント
+    @R0
+    M=M+1
+
+    //  FILEWHITEにジャンプ(ループ)
+    @FILLWHITE
+    0;JMP
+
+(FILLBLACK)
+    // R0の値によってジャンプ
+    @R0
+    D=M
+    @24575    // スクリーンの最後のマップメモリアドレス
+    D=D-A    // R0-24575
+    @LOOP
+    D;JEQ    // 0の場合ジャンプ
+    // R0に保存されているアドレスの値を1111111111111111(-1)にする
+    @R0
+    A=M
+    D=0
+    M=!D
+    //  R0をインクリメント
+    @R0
+    M=M+1
+
+    //  FILEBLACKにジャンプ(ループ)
+    @FILLBLACK
+    0;JMP
