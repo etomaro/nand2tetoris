f958fc327b83ede465d9c9203f57d465d82725b0
offerInc <offer1201@gmail.com>
2022-11-09 16:46:05 +0900

    04-fillおそらく完成。テストはやり方がわからなかった

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -10,6 +10,47 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+// keyboardを押したときM[24576]には値が入る。ここの処理は書かなくていい。
 
 // Put your code here.
-
+// keyが押されているとき(BLACK)に遷移。押されていないとき(WHITE)に遷移。
+// M[0]: スクリーンのアドレスを指定
+(LOOP)
+    @16384
+    D=A 
+    @0
+    M=D
+    @24576
+    D=M
+    @WHITE 
+    D;JEQ  // D=0の時WHITEに遷移
+    @BLACK
+    D;JNE  // D≠0の時BLACKに遷移
+(BLACK)
+    @0
+    D=M
+    @24576
+    D=D-A
+    @LOOP
+    D;JEQ  // R0=24576の時ジャンプ
+    @0
+    A=M 
+    M=-1
+    @0 
+    M=M+1  // インクリメント
+    @BLACK 
+    0;JMP  
+(WHITE)
+    @0
+    D=M
+    @24576
+    D=D-A
+    @LOOP
+    D;JEQ  // R0=24576の時ジャンプ
+    @0
+    A=M 
+    M=0
+    @0 
+    M=M+1  // インクリメント
+    @WHITE 
+    0;JMP   
